e75fc2765c82ecf7726e1407a7fb7bfa7dff0262
bullet28 <bullet28@mail.ua>
2014-11-12 03:28:29 +0200

    stream forward client pseudo

---
+++ src/main/java/com/nabusdev/padmedvbts2/model/Client.java
@@ -0,0 +1,49 @@
+package com.nabusdev.padmedvbts2.model;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Client {
+    private int clientID;
+    private Socket channel;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private static volatile int clientLastID = 0;
+    private Map<Integer, Client> clients = new HashMap<>();
+
+    public Client(Socket sChannel) {
+        this.channel = sChannel;
+        this.dataInputStream = createDataInputStream();
+        this.dataOutputStream = createDataOutputStream();
+        clientID = ++clientLastID;
+        this.addToList();
+    }
+
+    private DataInputStream createDataInputStream(){
+        try{ return new DataInputStream(getChannel().getInputStream()); }
+        catch (IOException e) {e.printStackTrace();}
+        return null;
+    }
+
+    private DataOutputStream createDataOutputStream(){
+        try{ return new DataOutputStream(getChannel().getOutputStream()); }
+        catch (IOException e) {e.printStackTrace();}
+        return null;
+    }
+
+    private void addToList() {
+        clients.put(getId(), this);
+    }
+
+    public int getId(){
+        return clientID;
+    }
+
+    public Socket getChannel() {
+        return channel;
+    }
+}
--- src/main/java/com/nabusdev/padmedvbts2/service/ForwardingService.java
+++ src/main/java/com/nabusdev/padmedvbts2/service/ForwardingService.java
@@ -1,5 +1,108 @@
 package com.nabusdev.padmedvbts2.service;
+import com.nabusdev.padmedvbts2.model.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 
 public class ForwardingService {
+    private static Logger logger = LoggerFactory.getLogger(ForwardingService.class);
+    private static final int TEMP_PORT = 27000;
+    private static final int TIMEOUT_SEC = 25;
+    public void init() {
+        try (ServerSocket network = new ServerSocket(TEMP_PORT)){
+            logger.info("Accepting connections at port " + port);
+            while (true){
+                new Thread(new Channel(network.accept())).start();
+            }
+        }
+        catch (BindException e){
+            logger.error("Failed to bind port " + TEMP_PORT);
+            logger.error(e.getMessage());
+            System.exit(1);
+        }
+        catch (IOException e) {e.printStackTrace();}
+    }
 
+    class Channel implements Runnable {
+        private Client client;
+        private Socket channel;
+        private int timeoutCount = 0;
+
+        public Channel(Socket channel){
+            this.client = new Client(channel);
+            this.channel = channel;
+        }
+
+        public void run(){
+            new Thread(new ChannelListen(client, channel)).start();
+            try{
+                DataOutputStream disOut = client.getDataOutputStream();
+                while (!channel.isClosed()){
+                    try {Thread.sleep(1000);}
+                    catch (Exception e) {e.printStackTrace();}
+                    try {
+                        disOut.writeByte(0);
+                        disOut.flush();
+                        timeoutCount = 0;
+                    }
+                    catch (Exception e){
+                        timeoutCount++;
+                        if (timeoutCount == TIMEOUT_SEC){
+                            dropChannel(channel, DisconnectReason.TIMEOUT);
+                        }
+                    }
+                }
+            }
+            catch (Exception e) {e.printStackTrace();}
+        }
+    }
+
+    class ChannelListen implements Runnable{
+        private Socket channel;
+        private Client client;
+
+        public ChannelListen(Client client, Socket channel){
+            this.channel = channel;
+            this.client = client;
+        }
+
+        public void run(){
+            String inStreamData;
+            DataInputStream inStream = client.getDataInputStream();
+            while (!channel.isClosed()){
+                try {
+                    inStreamData = inStream.readUTF();
+                    PacketParser.parseNetworkPacket(client, inStreamData);
+                }
+                catch (Exception e) {/* Ignoring "Connection Reset" Exception */}
+            }
+        }
+    }
+
+    public static void sendPacket(Client client, String packet){
+        try{
+            DataOutputStream outputStream = client.getDataOutputStream();
+            outputStream.writeUTF(packet);
+            outputStream.flush();
+        }
+        catch (SocketException e) {/* Ignoring "Socket Write Error" Exception */}
+        catch (IOException e) {e.printStackTrace();}
+    }
+
+    public static void dropChannel(Socket channel, DisconnectReason reason){
+        Client client = Client.getClient(channel);
+        fireClientDisconnectEvent(client, reason);
+        client.clientDisconnected();
+        AbstractClient abstractClient = AbstractClient.getAbstractClient(client);
+        if (abstractClient != null) abstractClient.abstractClientDropped();
+        try {channel.close();}
+        catch (IOException e) {e.printStackTrace();}
+    }
 }
